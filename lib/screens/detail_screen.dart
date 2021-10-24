5606e94513dc9a6ab561b8fd7bbb683a19f4fdd8
CarlosCastroG <carlos-castrog@hotmail.com>
2021-10-23 22:33:55 -0500

    Main Screen and Detailed Screen

---
+++ lib/screens/detail_screen.dart
@@ -0,0 +1,113 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+import 'package:harry_potter_api_app/models/character_api.dart';
+
+class CharacterDetails extends StatelessWidget {
+  final Character character;
+  const CharacterDetails({
+    Key? key,
+    required this.character,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.black,
+        title: Center(
+          child: Text('Detalle del Meme',
+              style: TextStyle(
+                fontSize: 27,
+                fontWeight: FontWeight.bold,
+              )),
+        ),
+      ),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Container(
+            height: 250,
+            width: 250,
+            margin: const EdgeInsets.only(top: 40),
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(80),
+              image: DecorationImage(
+                  image: NetworkImage(character.image), fit: BoxFit.cover),
+            ),
+          ),
+          const SizedBox(
+            height: 10,
+          ),
+          Container(
+            padding: const EdgeInsets.symmetric(horizontal: 5),
+            child: Column(
+              children: [
+                Text(
+                  character.name,
+                  style: const TextStyle(
+                      color: Colors.grey,
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold),
+                  maxLines: 2,
+                  overflow: TextOverflow.ellipsis,
+                ),
+                const SizedBox(
+                  height: 5,
+                ),
+                Row(
+                  children: [
+                    const Text(
+                      'Autor',
+                      style: TextStyle(fontWeight: FontWeight.bold),
+                    ),
+                    const SizedBox(
+                      width: 10,
+                    ),
+                    Text(character.house)
+                  ],
+                ),
+                const SizedBox(
+                  height: 10,
+                ),
+                Row(
+                  children: [
+                    const Text(
+                      'Fecha de creación',
+                      style: TextStyle(fontWeight: FontWeight.bold),
+                    ),
+                    const SizedBox(
+                      width: 10,
+                    ),
+                    Text(character.ancestry.toString())
+                  ],
+                ),
+                const SizedBox(
+                  height: 10,
+                ),
+                Row(
+                  children: [
+                    const Text(
+                      'Link',
+                      style: TextStyle(fontWeight: FontWeight.bold),
+                    ),
+                    const SizedBox(
+                      width: 10,
+                    ),
+                    Container(
+                      child: Text(
+                        character.actor.substring(0, 50),
+                        maxLines: 2,
+                        overflow: TextOverflow.ellipsis,
+                      ),
+                    )
+                  ],
+                ),
+              ],
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
--- lib/screens/main_screen.dart
+++ lib/screens/main_screen.dart
@@ -1,45 +1,130 @@
-// ignore_for_file: prefer_const_constructors
-
+import 'dart:convert';
+import 'package:connectivity/connectivity.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:adaptive_dialog/adaptive_dialog.dart';
+import 'package:harry_potter_api_app/helpers/constans.dart';
 import 'package:http/http.dart' as http;
 
-import 'package:harry_potter_api_app/helpers/constans.dart';
 import 'package:harry_potter_api_app/models/character_api.dart';
-import '';
+import 'package:harry_potter_api_app/screens/detail_screen.dart';
 
 class MainScreen extends StatefulWidget {
-  const MainScreen({ Key? key }) : super(key: key);
+  const MainScreen({Key? key}) : super(key: key);
 
   @override
   _MainScreenState createState() => _MainScreenState();
 }
 
 class _MainScreenState extends State<MainScreen> {
-List<Character> _characters = [];
+  List<Character> results = [];
 
   @override
   void initState() {
     super.initState();
-    _getCharacters();
+    _getApi();
+  }
+
+  Future _getApi() async {
+    var connectivityResult = await Connectivity().checkConnectivity();
+    if (connectivityResult == ConnectivityResult.none) {
+      setState(() {});
+      await showAlertDialog(
+          context: context,
+          title: 'Tiene un Error!',
+          message: 'Verifica que estes conectado a internet.',
+          actions: <AlertDialogAction>[
+            const AlertDialogAction(key: null, label: 'Aceptar'),
+          ]);
+      return;
+    } else {
+      var url =
+          Uri.parse(Constans.apiUrl);
+          Map<String, String> headers = {
+          "Content-type": "application/json",
+          "Accept": "application/json"
+        };
+      var response = await http.get(url, headers: headers);
+      final decodeData = json.decode(response.body);
+
+      if (decodeData['data'] != null && decodeData['data'].length > 0) {
+        for (var i = 0; i < decodeData['data'].length; i++) {
+          var image = decodeData['data'][i]['submission_url'];
+          if (image.contains('jpg') ||
+              image.contains('png') ||
+              image.contains('gif')) {
+            results.add(Character.fromJson(decodeData['data'][i]));
+          }
+        }
+
+        setState(() {});
+      }
+    }
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Harry Potter Characters')
+        backgroundColor: Colors.black,
+        title: Center(
+          child: Text('Harrey Potter (personajes)',
+              style: TextStyle(
+                fontSize: 27,
+                fontWeight: FontWeight.bold,
+              )),
+        ),
       ),
-      body: _getBody(),
+      body: ListImage(results: results),
     );
-  }
-
-  _getBody() {}
-
-  void _getCharacters() async{
-    var url = Uri.parse(Constans.apiUrl);
-    var response = await http.get(url);
   }
 }
 
+class ListImage extends StatelessWidget {
+  final List<Character> results;
 
+  const ListImage({
+    Key? key,
+    required this.results,
+  }) : super(key: key);
 
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+        itemCount: results.length,
+        itemBuilder: (_, i) {
+          return Container(
+              padding: const EdgeInsets.all(8),
+              margin: const EdgeInsets.only(bottom: 10),
+              child: Column(
+                children: [
+                  GestureDetector(
+                    onTap: () {
+                      Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) =>
+                                  CharacterDetails(character: results[i])));
+                    },
+                    child: Container(
+                      width: double.infinity,
+                      child: FadeInImage.assetNetwork(
+                        placeholder: 'assets/loading.gif',
+                        image: results[i].image,
+                        fit: BoxFit.cover,
+                        height: 200.0,
+                      ),
+                    ),
+                  ),
+                  const SizedBox(height: 5),
+                  Text(
+                    results[i].name,
+                    textAlign: TextAlign.center,
+                    style: const TextStyle(
+                        fontSize: 16, fontWeight: FontWeight.bold),
+                  ),
+                ],
+              ));
+        });
+  }
+}
